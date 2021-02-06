da746097581eef8b2776015b30997c2cc1648c4f
Devon G <devgough@gmail.com>
2021-02-05 22:31:03 -0500

    Added checkPos

--- nqueens.py
+++ nqueens.py
@@ -1,16 +1,31 @@
-
-import random
-
-
 # Implement a solver that returns a list of queen's locations
 #  - Make sure the list is the right length, and uses the numbers from 0 .. BOARD_SIZE-1
 def solve(board_size):
 
-    # This almost certainly is a wrong answer!
-    answer = list(range(1,board_size+1))
-    random.shuffle(answer)
+    # Creating the board.
+    board = [[0 for col in range(board_size)] for row in range(board_size)]
 
-    #Aliya's comment
-    #James's comment
+    # TODO: Create algo for randomly putting queens on board
+    # TODO: Implement this: 
+    # A reasonably good starting point can, 
+    # for instance, be found by putting each queen in its own row and column so that it conflicts with the smallest number of queens already on the board
 
-    return answer
+# Checks to see if a board space is available   
+def checkPos(row, col, board):
+    # Checks row/column
+    for i in range(len(board)):
+        if board[row][i] == 1 or board[i][col]== 1:
+            return False
+
+        # Checks the diagonal
+        for j in range(len(board)):
+            if (i+j == row+col) or (i-j == row-col):
+                if board[i][j] == 1:
+                    return False
+    return True
+
+
+
+
+if __name__ == "__main__":
+    solve(4)
