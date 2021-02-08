875b2a44fa4ea0aa5bbcba9d7f97cf646a3cffdf
16fd7 <78316850+16fd7@users.noreply.github.com>
2021-02-08 00:20:45 -0500

    Update nqueens.py

--- nqueens.py
+++ nqueens.py
@@ -7,35 +7,48 @@ import timeit
 #  - Make sure the list is the right length, and uses the numbers from 0 .. BOARD_SIZE-1
 def solve(board_size):
 
-    # Creating the board.
+    # Creating the chess board
     board = [[0 for col in range(board_size)] for row in range(board_size)]
     for i in range(len(board)):
         board[i][i] = 1
 
     counter = 0;
-
+    
+    # Measures the starting execution time 
     start = timeit.default_timer()
 
     while True:
-        board = randomShuffle(board_size)
-        board = initializeBoard(board)
+        board = randomShuffle(board_size) # Randomly places N queens on the board
+        board = initializeBoard(board) # Rearranges the positions of N-queens without any conflicts
+        
         for i in range(0, 60):
             counter = counter + 1;
             answer = converter(board, board_size)
             if solution(answer, board_size):
-                stop = timeit.default_timer()
-                print("Swaps: " + str(counter) + " Time: " + str(stop - start))
+                stop = timeit.default_timer() # Measures the stopping execution time
+                
+                # Prints runtime - how long it takes to perform a series of swaps
+                print("Swaps: " + str(counter) + " Time: " + str(stop - start)) 
                 return answer
+            
+            # A list determining the positions (i,j) of current queens on the board
             queens = findQueens(board)
             board = minConflicts(board, 1, queens)
 
+# TO REVISE!!
+# Rearranges the positions of queens into different rows and columns with the least amount of possible conflicts
 def initializeBoard(board):
-    queens = findQueens(board);
+    
+    # A list determining the positions (i,j) of current queens on the board
+    queens = findQueens(board)
+    
     for i in range(len(board)):
-        row = queens[i][0]
-        column = queens[i][1]
+        row = queens[i][0] # Determines which row each queen is currently in
+        column = queens[i][1] # Determines which column each queen is currently in
         minimumConflict = len(board)
         rowList = []
+        
+        # Places queens into different columns with least possible conflicts
         for j in range(len(board)):
             currentConflict = checkConflicts(j, column, board)
             if currentConflict < minimumConflict:
@@ -44,6 +57,7 @@ def initializeBoard(board):
             elif currentConflict == minimumConflict:
                 rowList.append(j)
 
+        # Selects a random row from the suitable rowList
         newRow = r.choice(rowList)
 
         board[row][column] = 0
@@ -51,7 +65,7 @@ def initializeBoard(board):
 
     return board
 
-
+# Converts the chess board to a row vector
 def converter(board, boardSize):
 
     rowVector = [];
